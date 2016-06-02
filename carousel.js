4c1fd305cc74a401370c8a38df26fc9e57e3b7ca
moller-g-peter <moller.g.peter@gmail.com>
2016-06-02 12:19:34 +0200

    issues with swipe function and active tabs. css issues corrected

--- carousel.js
+++ carousel.js
@@ -14,21 +14,55 @@ $("#myCarousel").swipeleft(function() {
 
 
 var clickEvent = false;
+// when you click the a tag, do following:
 $('#myCarousel').on('click', '.nav a', function() {
     clickEvent = true;
+
+    // remove 'active' from previous button
     $('.nav li').removeClass('active');
+
+    // add 'active' on the button you pressed
     $(this).parent().addClass('active');
+
+    // after one slide is finished, do following:
 }).on('slid.bs.carousel', function(e) {
+
+  // right now clickEvent is false
   if(!clickEvent) {
+
+    // this counts how may slides ('item' divs) we have (0,1,2)
     var count = $('.nav').children().length -1;
+
+    
     var current = $('.nav li.active');
+
+    // remove 'active' from current and add it to the next
     current.removeClass('active').next().addClass('active');
+
+    // parseInt makes string "data-slide-to=0" to int 0
     var id = parseInt(current.data('slide-to'));
+
+    // if slide amount is the same as "data-slide-to= num":
     if(count == id) {
+
+      // add 'active' to the first li element in the list
       $('.nav li').first().addClass('active');
     }
   }
+
+  // reset the click event
   clickEvent = false;
 });
+
+
+
+
+
+
+
+
+
+
+
 
 
