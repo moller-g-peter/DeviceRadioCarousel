640b65bb09928556efe0055cc6752efcbe7af20b
moller-g-peter <moller.g.peter@gmail.com>
2016-06-20 09:29:21 +0200

    Christian and I decided to go for the current order "stream - chat - ide" due to difficulties to move it.

--- carousel.js
+++ carousel.js
@@ -47,53 +47,69 @@ $('.nav li').click(function(){
 });
 
 
-//$('#myCarousel').carousel({
-//    interval: 2000
-//});
-
-// $(function() {
-//   $("#notshurethisworks").swipe( {
-//     //Generic swipe handler for all directions
-//     swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-//       $(this).text("You swiped " + direction );
-//     }
-//   });
-
-//   //Set some options later
-//   // $("#test").swipe( {fingers:2} );
-// });
-
-
-
-
-
 $('.exe_button_1').hide();
 $('.exe_button_2').hide();
 $('.exe_button_3').hide();
 $('.reload_exe').hide();
 
+// $(window).resize(function(){
+//   if ($(window).width() > 1025) {
+
+//     // $('#chatDiv').append('<div id="chatlioWidgetPlaceholder" class="chatlioDiv" style="margin: auto;"></div>');
+
+//     $('#putMeInside').append('<script src="chatlio.js"></script>');
+//   }
+// }).resize();
+
+
+
 $(window).resize(function(){
   if ($(window).width() < 1025) {
     var cnt = $(".desktopOnly").contents();
     $(".desktopOnly").replaceWith(cnt);
-    // console.log("vad är ditt problem");
-    $('.chatDiv').detach();
-    
-    if($( ".chatDiv" ).length>0 ){
-        
-    //  alert("not removed");  
-        
-    }else{
-        $( ".myDiv" ).insertBefore( $( ".beforeDiv" ) );
-        
-           $(".myDiv").append('<div class="col-sm-12 item chatDiv"><div id="chatlioWidgetPlaceholder" class="chatlioDiv" style="margin: auto;"></div></div>');
-        alert("removed");
-    }
-            //.insertBefore('.beforeDiv');
-   // $( ".logDiv" ).appendTo( $( ".ideDiv" ) );
+
+    // $('#chatDiv').insertBefore('.beforeDiv');
+    $( ".logDiv" ).appendTo( $( ".ideDiv" ) );
+    // $('#putMeInside').append('<script src="chatlio.js"></script>');
+    // $('#chatDiv').append('<div id="chatlioWidgetPlaceholder" class="chatlioDiv" style="margin: auto;"></div>');
+    // $('#putMeInside').append('<script src="chatlio.js"></script>');
+
+    // var removeChat = $("#chatlioWidgetPlaceholder").contents();
+    // $("#chatlioWidgetPlaceholder").replaceWith(removeChat);
+
+    // $('#chatlioWidgetPlaceholder').remove();
+    // $('#chatDiv').append('<div id="chatlioWidgetPlaceholder" class="chatlioDiv" style="margin: auto;"></div>');
+    // $('.chatDiv').detach().insertBefore('.beforeDiv');
+
+    // $('.ideDiv').before('.beforeDiv');
+
+    // $('.chatDiv').each(function() {
+    //     $(this).insertAfter($(this).parent().find('.ideDiv'));
+    // });
     // $( ".logDiv" ).appendTo( '$( ".ideDiv" ');
   }
+  // chatlioStart();
 }).resize();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -213,6 +229,14 @@ $(window).resize(function(){
 
 
 
+// function chatlioStart(){
+//   window._chatlio = window._chatlio||[];
+//    !function(){ var t=document.getElementById("chatlio-widget-embed");if(t&&window.ChatlioReact&&_chatlio.init)return void _chatlio.init(t,ChatlioReact);for(var e=function(t){return function(){_chatlio.push([t].concat(arguments)) }},i=["configure","identify","track","show","hide","isShown","isOnline"],a=0;a<i.length;a++)_chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));var n=document.createElement("script"),c=document.getElementsByTagName("script")[0];n.id="chatlio-widget-embed",n.src="https://w.chatlio.com/w.chatlio-widget.js",n.async=!0,n.setAttribute("data-embed-version","2.1");
+//       n.setAttribute('data-widget-options', '{"embedInline": true}');
+//       n.setAttribute('data-widget-id','e7107c6c-1bf3-4c0d-5eb6-f151b4e491c8');
+//       c.parentNode.insertBefore(n,c);
+//    }();
+//  }
 
 
 
