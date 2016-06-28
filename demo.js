681373e7f40df7af39c6051417f7b86aa919c38b
moller-g-peter <moller.g.peter@gmail.com>
2016-06-28 13:47:58 +0200

    working on final touches

--- demo.js
+++ demo.js
@@ -442,6 +442,7 @@ $(function () {
 // event handler for disconnection
     live.on('disconnect', function () {
         $('.btn').addClass('disabled');
+        alert('hello!!');
     });
 
 // event handler for successfull firmware upload
@@ -550,7 +551,7 @@ live.connect();
           if (!$(this).hasClass('disabled')) {
             // alert(live.queueing);
             if (!live.queueing) {
-                $('.btn').addClass('disabled');
+                // $('.btn').addClass('disabled');
                 // put us in queue
                 live.queue();
                 //   alert(live.queueing);
@@ -564,7 +565,7 @@ live.connect();
                 setTimeout(function () {
                     $("#myModalNotification").modal('toggle');
                 }, 2000);
-                $('.btn').prop("disabled", true);
+                // $('.btn').prop("disabled", true);
 
                 // $('.disabledButtonMessage').show();
                 $('.logDiv').prepend('<button class="disabledButtonMessage"></button>');
