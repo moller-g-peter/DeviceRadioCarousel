077e821078b80344eeb527e259386d5b3ae19db4
shamrat asfaq <shamrat asfaq>
2016-06-28 11:19:47 +0200

    shamrat code fixes in JS file

--- demo.js
+++ demo.js
@@ -10,9 +10,13 @@ $('.logDiv').append('<div class="disabledWrapper"></div>');
 var editorvalue = "";
 var editor="";
 var isOnQue=false;
+var buttonBoolean = true;
+var markers_present = [];
+var program_b64 = null;
+var compiled=false;
+var function_starttime=null;
 
-
-     var markers_present = [];
+    
 
 function ace_grammar_demo(_editor, code, langs)
 {
@@ -196,16 +200,6 @@ function ace_grammar_demo(_editor, code, langs)
     return editor;
 }
 
-
-
-
-
-
-var buttonBoolean = true;
-
-
-
-
 function excButtonQueue() {
 
           
@@ -248,8 +242,6 @@ function excButtonQueue() {
 
 }
 
-
-
 function reloadButton() {
     
 
@@ -278,34 +270,12 @@ function reloadButton() {
      // $('.reload_default').show();
 }
 
-
- 
-
-
-
-
-
-
-  var program_b64 = null;
-  var compiled=false;
-  var function_starttime=null;
-
 function deviceradioProcess(text) {
   function_starttime = new Date();
  
-    
-    
 $('#console').html('');
 
-
-    //console.log(" i am here"+text);
-
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
-
-
-
-
- 
 
 
     var data = text;
@@ -525,7 +495,7 @@ $(function () {
                 $('.reload_button_disabled').hide();
                 $('.exe_button_default').show();
                 $('.reload_default').show();
-                // $('.exe_button_default').css('display','block');
+                $('.exec_button').removeAttr('data-target');
 
               
                   
@@ -540,9 +510,9 @@ $(function () {
            //  alert('ur turn hurrah');
                 $('#myModalNotification').modal('show');
                 $('#modalMessages').html('It is now your time to have control of the device');
-                  setTimeout(function(){
-                   $("#myModalNotification").modal('toggle');
-                  }, 2000);
+//                  setTimeout(function(){
+//                   $("#myModalNotification").modal('toggle');
+//                  }, 2000);
         } else {
             $('#console').prepend('<p>Your turn is up</p>');
                 $('#myModalNotification').modal('show');
@@ -550,6 +520,10 @@ $(function () {
                     setTimeout(function(){
                    $("#myModalNotification").modal('toggle');
                   }, 2000);
+                  
+                  $('.exec_button').attr('data-target','#myModalExecute');
+                    $('.btn').removeAttr('disabled');
+                  
           //  alert('Your time is now up');
         }
     });
@@ -641,8 +615,6 @@ live.connect();
 
 
 });
-
-
 
 
 $('.exe_button_disabled, .reload_button_disabled').click(function(){
