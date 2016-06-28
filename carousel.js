bee4354d195a359c843c62eb667af59e82cf4df4
moller-g-peter <moller.g.peter@gmail.com>
2016-06-28 11:18:04 +0200

    ...

--- carousel.js
+++ carousel.js
@@ -104,26 +104,9 @@ $(window).resize(function(){
 $(window).resize(function(){
   if ($(window).width() < 645 && $(window).width() > 365) {
     $('.modal-footer').css('padding', '0');
+    // $('.newsletterButton').css('bottom', '50');
   }
 }).resize();
-
-
-// .modal-footer{
-//     padding: 0 !important;
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -228,28 +211,6 @@ $(window).resize(function(){
 //   clickEvent = false;
 // });
 
-
-
-
-
-
-
-
-
-
-// Create the orange background(focused div)
-
-
-
-
-// function chatlioStart(){
-//   window._chatlio = window._chatlio||[];
-//    !function(){ var t=document.getElementById("chatlio-widget-embed");if(t&&window.ChatlioReact&&_chatlio.init)return void _chatlio.init(t,ChatlioReact);for(var e=function(t){return function(){_chatlio.push([t].concat(arguments)) }},i=["configure","identify","track","show","hide","isShown","isOnline"],a=0;a<i.length;a++)_chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));var n=document.createElement("script"),c=document.getElementsByTagName("script")[0];n.id="chatlio-widget-embed",n.src="https://w.chatlio.com/w.chatlio-widget.js",n.async=!0,n.setAttribute("data-embed-version","2.1");
-//       n.setAttribute('data-widget-options', '{"embedInline": true}');
-//       n.setAttribute('data-widget-id','e7107c6c-1bf3-4c0d-5eb6-f151b4e491c8');
-//       c.parentNode.insertBefore(n,c);
-//    }();
-//  }
 
 
 
