87f7b16b8b0d327d921e443597eab411d5c4192e
moller-g-peter <moller.g.peter@gmail.com>
2016-06-29 12:26:20 +0200

    progress bar updates

--- carousel.js
+++ carousel.js
@@ -144,76 +144,13 @@ $(window).resize(function(){
 }).resize();
 
 
-// var clickEvent = false;
-// // when you click the a tag, do following:
-// $('#myCarousel').on('click', '.nav a', function() {
-//     clickEvent = true;
-
-//     // remove 'active' from previous button
-//     $('.nav li').removeClass('active');
-
-//     // add 'active' on the button you pressed
-//     $(this).parent().addClass('active');
-
-//     // after one slide is finished, do following:
-// }).on('slid.bs.carousel', function(e) {
-
-//   // right now clickEvent is false
-//   if(!clickEvent) {
-
-
-//     // this counts how may slides ('item' divs) we have (0,1,2)
-//     var count = $('.nav').children().length -1;
-//     var direction=$('.item').html();
-//     if($(".carousel-inner > .item").hasClass("active")){
-        
-//      //   alert($(".carousel-inner > .item").attr('id'));
+// function validateEmail() {
+//     var x = document.forms["newsletterForm"]["eMail"].value;
+//     if (x === null || x === "") {
+//         alert("E-mail must be filled out");
+//         return false;
 //     }
-
-
-//     ///fake change
-//     var current = $('.nav li.active');
-//     var carosalClass=$('.col-xs-4');
-
-//     // remove 'active' from current and add it to the next
-//    carosalClass.removeClass('active');
-   
-
-
-
-//     // parseInt makes string "data-slide-to=0" to int 0
-//     var id = parseInt(current.data('slide-to'));
-    
-    
-// var totalItems = $('.item').length;
-// var currentIndex = $('div.active').index();
-
-
-// $('.col-xs-4[data-slide-to="'+currentIndex+'"]').addClass('active');
-
-// //  }
-
-//   // console.log('current index', ''+currentIndex);
-//     //console.log('current: ', current);
-
-//     // if slide amount is the same as "data-slide-to= num":
-//     if(count == id) {
-
-// //console.log('i am if');
-//       // add 'active' to the first li element in the list
-//      // $('.nav li').first().addClass('active');
-//     }else{
-
-//     //  console.log('else');
-//     }
-//   }
-
-//   // reset the click event
-//   clickEvent = false;
-// });
-
-
-
+// }
 
 
 
